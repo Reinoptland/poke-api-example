b8697b39f9dbc9e986545b22a82844c5655a8d2c
Reinoptland <reinoptland@gmail.com>
2020-12-08 20:17:57 +0100

    Async functie maken, axios & aanroepen,

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ function App() {
   const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
-    console.log("HALLO! Ik zit in useEffect");
+    // console.log("HALLO! Ik zit in useEffect");
+
+    // definieren (opschrijven)
+    async function getPokemonData() {
+      const result = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+      console.log(result.data);
+    }
+
+    getPokemonData(); // aanroepen
   }, []);
 
   return <div className="App">Hello world</div>;
