705ea0244ac599073dcbbf90576603bcfdb0b1ea
Reinoptland <reinoptland@gmail.com>
2020-12-11 14:43:33 +0100

    State updaten met de response van de api

--- src/App.js
+++ src/App.js
@@ -15,13 +15,23 @@ function App() {
           `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20`
         );
         console.log("DIT KREGEN WE TERUG:", response.data.results);
+        setPokemons(response.data.results);
+        // Werkt niet want het component moet nog gererendered worden :(
+        console.log("DIT WERKT OOK NIET:", pokemons);
       } catch (error) {}
     }
 
     getPokemons();
-
+    // Werkt niet want getPokemons is een async function :(
+    console.log("DIT WERKT NIET:", pokemons);
     console.log("hallo! Ik ben een effect");
   }, []);
+
+  // Vuistregel:
+  // Controleren wat de state op dit moment is
+  // altijd boven je return statement
+
+  console.log("WAT IS NU DE STATE", pokemons); // 2x uitgevoerd
 
   return (
     <div className="App">
