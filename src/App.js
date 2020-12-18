6ea64886eeb436eeb06d252910facb3787a5f241
Reinoptland <reinoptland@gmail.com>
2020-12-18 14:12:16 +0100

    UseState for loggedIn, pass as prop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import HomePage from "./components/HomePage";
+import { useState } from "react";
 import MyPokemons from "./components/MyPokemons";
 import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  console.log("BEN IK INGELOGD?", loggedIn);
+
   return (
     <>
       <nav>
@@ -11,9 +16,11 @@ function App() {
       </nav>
       <Switch>
         <Route path="/mypokemons">
-          <MyPokemons />
+          {/* ben je ingelogd */}
+          <MyPokemons loggedIn={loggedIn} />
         </Route>
         <Route exact path="/">
+          {/* inloggen */}
           <HomePage />
         </Route>
         <Route path="/">404 Page Not found, sorry!</Route>
--- src/components/MyPokemons.js
+++ src/components/MyPokemons.js
@@ -1,7 +1,13 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
 
-export default function MyPokemons() {
+export default function MyPokemons(props) {
+  console.log("PROPS IN MYPOKEMONS PAGE:", props);
+
+  if (props.loggedIn === false) {
+    return "GO BACK TO HOME PAGE PLEASE";
+  }
+
   return (
     <div>
       <PokemonCard name="rattata" />
