0f87261279508049352dd5bc20c2685c513f291c
Reinoptland <reinoptland@gmail.com>
2020-12-18 15:09:32 +0100

    Add buttons to navbar using context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import HomePage from "./components/HomePage";
 import { useState } from "react";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
 import MyPokemons from "./components/MyPokemons";
 import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  console.log("BEN IK INGELOGD?", loggedIn);
-
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/mypokemons">My Pokemon</Link>
-      </nav>
+      <NavBar />
       <Switch>
         <Route path="/mypokemons">
-          {/* ben je ingelogd */}
           <MyPokemons />
         </Route>
         <Route exact path="/">
-          {/* inloggen */}
           <HomePage />
         </Route>
         <Route path="/">404 Page Not found, sorry!</Route>
